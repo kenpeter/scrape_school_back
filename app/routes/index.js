805575c979a6f591b43f5831338e10bca58d5d6f
kenpeter <figo2476@hotmail.com>
2017-04-02 22:12:20 +1000

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,6 @@ const schoolRoute = require('./schoolRoute');
 // module exports func
 module.exports = function(app, db) {
   // call the routes
-  productRoute(app, db);
+  schoolRoute(app, db);
   // Other route groups could go here, in the future
 };
